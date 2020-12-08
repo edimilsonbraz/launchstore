57e7d15806926b1b7dad330f0bbc1326c775ddc9
edimilson braz <edimilsonbraz@pop-os.localdomain>
2020-12-08 17:57:58 -0300

    construindo e estilizando carrinho

---
+++ src/routes/cart.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const routes = express.Router()
+
+const CartController = require('../app/controllers/CartController')
+
+
+routes.get('/', CartController.index)
+
+
+
+module.exports = routes
+
