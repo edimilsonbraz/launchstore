15cec77fdfd66b3e5d3717bf8bffe463a2c6dfc3
edimilson braz <edimilsonbraz@pop-os.localdomain>
2020-12-08 22:31:41 -0300

    removendo itens do carrinho

--- src/app/controllers/CartController.js
+++ src/app/controllers/CartController.js
@@ -5,12 +5,10 @@ const LoadProductsService = require('../services/LoadProductService')
 module.exports = {
     async index(req, res) {
         try {
-            const product = await LoadProductsService.load('product', {where: { id: 1}})
-
             let { cart } = req.session
 
             // gerenciador de carrinho
-            cart = Cart.init(cart).addOne(product)
+            cart = Cart.init(cart)
 
             return res.render('cart/index', {cart})
 
@@ -18,5 +16,43 @@ module.exports = {
             console.error(error);
         }
 
+    },
+    async addOne(req, res) {
+        //pegar o id do produto e o produto
+        const { id } = req.params
+
+        const product = await LoadProductsService.load('product', {where: { id }})
+
+        //pegar o carrinho da sessão
+        let { cart } = req.session
+
+        //adicionar o produto ao carrinho (usando nosso gerenciador de carrinho)
+        cart = Cart.init(cart).addOne(product)
+
+        //atualizar o carrinho da sessão
+        req.session.cart = cart
+
+        //redirecionar o usuário para a tela do carrinho.
+        return res.redirect('/cart')
+
+    },
+    removeOne(req, res) {
+        // pegar o id do produto
+           let {id} = req.params
+
+        // pegar o carrinho da sessão
+            let {cart} = req.session
+
+        // Se não tiver carrinho, retornar
+            if(!cart) return res.redirect('/cart')
+
+        // iniciar o carrinho(gerenciador de carrinho) e remover
+            cart = Cart.init(cart).removeOne(id)
+
+        // Atualizar os carrinho da sessão, removendo 1 item
+            req.session.cart = cart
+
+        //redirecionamento para a pagina cart
+            return res.redirect('/cart')
     }
 }
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -5,7 +5,8 @@ const CartController = require('../app/controllers/CartController')
 
 
 routes.get('/', CartController.index)
-
+routes.post('/:id/add-one', CartController.addOne)
+routes.post('/:id/remove-one', CartController.removeOne)
 
 
 module.exports = routes
